1eadca0ea2337197efdac094dd822e56ae80f23a
talk2sameer2007-oss <talk2sameer2007@gmail.com>
2026-07-17 13:03:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ interface = gr.Interface(
     ],
     outputs=gr.Text(label="Assessment Result"),
     title="Diabetes Prediction System",
-    description="Enter the medical metrics to predict diabetes risk using a Decision Tree Machine Learning model."
+    description="Enter the medical metrics to predict diabetes risk using a Decision Tree Machine Learning model.\NAME-SAMEER, ROLLNO.-241020"
 )
 # ------------------------------------------
 
